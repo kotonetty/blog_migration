b954639ba706f9e5ff55f4c5857670ea15604b08
kikuchik <kikuchik@linux4.amelieff.local>
2016-04-11 12:47:29 +0900

    画像をローカルに保存する処理の途中まで

--- bizlog.rb
+++ bizlog.rb
@@ -1,6 +1,12 @@
+require 'rubygems'
 require 'nokogiri'
 require 'date'
 require 'time'
+require 'open-uri'
+require 'aws-sdk-core'
+
+Aws.config[:region] = 'ap-northeast-1'
+s3 = Aws::S3::Client.new
 
 #XMLからMovable Typeに変換するプログラム
 file = File.open("bizlog.xml")
@@ -16,6 +22,46 @@ blogs.each do |blog|
 	change_date = DateTime.parse(date)
 	date_time = change_date.strftime(date_format) #dateの形式を合わせる
 	body = blog.xpath("description").text
+	if body.scan(/<img src=\".*\".*\/>/) != nil then
+		body_images = body.scan(/<img src=\".*\".*\/>/)
+		body_images.each do |body_image|
+			image_path = ""
+			if body_image.match(/\"images\/.*g\"\s/) != nil
+				change_image = body_image.match(/\"images\/.*g\"\s/)
+				image_path = change_image[0].gsub(/images/,'http://blog.amelieff.jp/images')
+				body_after = body.gsub(/images/,'http://blog.image.s3.amazonaws.com')
+			elsif body_image.match(/\"amelieff.*g\"/)
+				change_image = body_image.match(/\"amelieff.*g\"/)
+				image_path = change_image[0]
+				body_after = body.gsub(/amelieff\.jp\/wp\/wp-content\/uploads/,'blog.image.s3.amazonaws.com')
+			end
+			if image_path != "" then
+				dir_name = "/home/kikuchik/bizlog/images/"
+#				file_open = File.open(image_path)
+				file_name = File.basename(image_path)
+				file_path = dir_name + file_name
+				open(file_path, 'rb') do |output|
+					open(image_path) do |data|
+						output.write(data.read)
+					end
+				end
+#				s3.puts_object(
+#					bucket: "blog.image",
+#					body: file_open,
+#					key: file_name
+#				)
+			end
+		end
+#		end
+#		file_open = File.open(blog_images_change)
+#		file_name= File.basename(blog_images_change)
+#		s3.put_object(
+#			bucket: "blog.image",
+#			body: file_open,
+#			key: file_name
+#		)	
+	end
+	
 	comment_list = []
 	if blog.xpath("comments").text != "" then #コメントがある場合
 		comments = blog.xpath("comments//comment")
@@ -49,36 +95,36 @@ blogs.each do |blog|
 	end 
 
 #Movable Typeに変換したものを書き出す
-	puts "AUTHOR:" + author 
-	puts "TITLE:" + title
-	puts "STATUS:Publish"
-	puts "ALLOW_COMMENTS: 1"  
-	puts "ALLOW_PINGS: 1"
-	puts "CONVERT BREAKS: 1"
-	puts "PRIMARY CATEGORY:" + primary_category
-	puts "DATE:" + date_time
-	puts "-----\nBODY:\n" + body + "\n-----"
-	puts "EXTENDED BODY:" + "\n-----"
-	puts "KEYWORDS:" + "\n-----"
-	if !comment_list.empty? then
-		comment_list.each do |comment|
-			puts "COMMENT:"
-			puts "AUTHOR:" + comment["author"]
-			puts "DATE:" + comment["date"].to_s
-			puts "EMAIL:" + comment["email"]
-			puts comment["body"] + "\n-----"
-		end
-	end
-	if !trackback_list.empty? then
-		trackback_list.each do |trackback|
-			puts "PING:"
-			puts "TITLE:" + trackback["title"]
-			puts "URL:" + trackback["url"]
-			puts "IP:" + trackback["ip"]
-			puts "BLOG NAME:" + trackback["blog_name"]
-			puts "DATE:" + trackback["date"]
-			puts trackback["excerpt"] + "\n-----"	
-		end	
-	end
-	puts "--------"
+#	puts "AUTHOR:" + author 
+#	puts "TITLE:" + title
+#	puts "STATUS:Publish":
+#	puts "ALLOW_COMMENTS: 1"  
+#	puts "ALLOW_PINGS: 1"
+#	puts "CONVERT BREAKS: 1"
+#	puts "PRIMARY CATEGORY:" + primary_category
+#	puts "DATE:" + date_time
+#	puts "-----\nBODY:\n" + body + "\n-----"
+#	puts "EXTENDED BODY:" + "\n-----"
+#	puts "KEYWORDS:" + "\n-----"
+#	if !comment_list.empty? then
+#		comment_list.each do |comment|
+#			puts "COMMENT:"
+#			puts "AUTHOR:" + comment["author"]
+#			puts "DATE:" + comment["date"].to_s
+#			puts "EMAIL:" + comment["email"]
+#			puts comment["body"] + "\n-----"
+#		end
+#	end
+#	if !trackback_list.empty? then
+#		trackback_list.each do |trackback|
+#			puts "PING:"
+#			puts "TITLE:" + trackback["title"]
+#			puts "URL:" + trackback["url"]
+#			puts "IP:" + trackback["ip"]
+#			puts "BLOG NAME:" + trackback["blog_name"]
+#			puts "DATE:" + trackback["date"]
+#			puts trackback["excerpt"] + "\n-----"	
+#		end	
+#	end
+#	puts "--------"
 end
